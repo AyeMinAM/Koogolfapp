5cbc36960c2e13b870d42c11d5c3bdc51efde176
Tuo <tuohope@gmail.com>
2018-12-16 20:02:07 -0800

    hooked account service

--- src/app/home/home/home.page.ts
+++ src/app/home/home/home.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../../services/account.service'
+
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+    selector: 'app-home',
+    templateUrl: './home.page.html',
+    styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+    constructor(private accountService: AccountService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+
+    hello(){
+        this.accountService.testMethod();
+    }
 
 }
