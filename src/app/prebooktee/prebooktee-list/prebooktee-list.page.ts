dde4791a63ec1e59c8d459e1d577d4d7d38fce56
Tuo <tuohope@gmail.com>
2018-12-16 19:53:26 -0800

    added partial ionic3 code by ayemin

--- src/app/prebooktee/prebooktee-list/prebooktee-list.page.ts
+++ src/app/prebooktee/prebooktee-list/prebooktee-list.page.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrebookteeListPage implements OnInit {
 
+  items = [
+        {
+            name: "Northview-Ridge球场",
+            about: "Northview Golf and Country Club - Ridge",
+            regularPrice : 55,
+            memberPrice : 45,
+            savePercentage : 10,
+            date: "2018年12月2日 星期日",
+        },
+        {
+            name: "天鹅海湾度假乡村俱乐部 — 度假村球场",
+            about: "Swan-e-set Bay Resort & Country Club - Resort",
+            regularPrice : 33,
+            memberPrice : 45,
+            savePercentage : 10,
+            date: "2018年12月4日 星期二",
+        },
+        {
+            name: "Meadow Gardens球场",
+            about: "Meadow Gardens Golf Course",
+            regularPrice : 55,
+            memberPrice : 45,
+            savePercentage : 10,
+            date: "2018年12月5日 星期三",
+        },
+        {
+            name: "Hazelmere球场",
+            about: "Hazelmere Country Club",
+            regularPrice : 55,
+            memberPrice : 45,
+            savePercentage : 10,
+            date: "2018年12月6日 星期四",
+        }
+    ]
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  itemTapped(item) {
+
+  }
 }
